3cd792e655b6d187f9d5348fda3df8e1fe4e9d89
Jesse-Moran <113071383+Jesse-Moran@users.noreply.github.com>
2024-09-30 19:05:57 -0500

    9302024

---
+++ MAGD 372 Assignments Project/Assets/CubeSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectPooler.SpawnFromPool("Cube", transform.position, Quaternion.identity);
+    }
+}
